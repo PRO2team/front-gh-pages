b92c97d4920bce5746932bc36dedb143374a086d
Oleksii Hudkov <oleksiihudkov@gmail.com>
2023-01-07 10:32:21 +0100

    fix path

--- src/components/PageHome/Hero/HeroVideo.js
+++ src/components/PageHome/Hero/HeroVideo.js
@@ -3,7 +3,7 @@ import Video from "../../Utility/Video";
 const HeroVideo = () => {
   return (
     <div className="hero-video">
-      <Video src="hero.mp4" />
+      <Video src={`${procces.env.PUBLIC_URL}/hero.mp4`} />
       <h2 className="hero-video__title">Book everything you want</h2>
     </div>
   );
