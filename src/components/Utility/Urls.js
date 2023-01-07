2958e4155df3e59c94c1df92167e592fc8e1b4ff
Oleksii Hudkov <oleksiihudkov@gmail.com>
2023-01-07 14:04:16 +0100

    small fixes

--- src/components/Utility/Urls.js
+++ src/components/Utility/Urls.js
@@ -2,16 +2,4 @@ const BASE_URL = "https://bebraapi.azurewebsites.net";
 
 export default {
   BASE_URL: BASE_URL,
-  APPOINTMENT_LIST_URL: BASE_URL + "/api/Appointments/list",
-  CONTACT__SUBSCRIPTION_URL: BASE_URL + "/api/Contact/subscription",
-  APPOINTMENT_CANCEL_URL: BASE_URL + "/api/Appointment/",
-  DELETE_FROM_WISHLIST_URL: BASE_URL + "/api/Accounts/favourites/",
-  CONTACT_URL: BASE_URL + "/api/Contact/contact",
-  LOGIN_URL: BASE_URL + "/api/Accounts/login",
-  REGISTER_URL: BASE_URL + "/api/Accounts/register",
-  REFRESH_TOKEN_URL: BASE_URL + "/api/Accounts/refresh",
-  CONTACT_APPLICATION_URL: BASE_URL + "/api/Contact/application",
-  APPOINTMENT_CANCEL_URL: BASE_URL + "/api/Appointment/cancel",
-  ACCOUNTS_WISHLIST_URL: BASE_URL + "/api/Accounts/",
-  GET_SALONS_URL: BASE_URL + "/api/Salons/",
 };
