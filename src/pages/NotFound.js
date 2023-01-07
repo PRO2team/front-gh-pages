5f42ecf294092d62157dee27a6561fe423b4cbd1
Oleksii Hudkov <oleksiihudkov@gmail.com>
2023-01-07 17:59:44 +0100

    added styles for not found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,10 +3,10 @@ import React from "react";
 import "react-multi-carousel/lib/styles.css";
 import "../sass/base/slider.scss";
 import "../sass/components/modal.module.scss";
-
+import "../sass/base/typography.scss";
 
 const NotFound = () => {
-  return <p>Not found</p>;
+  return <p className="not-found">This page not found</p>;
 };
 
 export default NotFound;
