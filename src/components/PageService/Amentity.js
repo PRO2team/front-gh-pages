3bf0bb6239cf8fcd88fbc1e08e26847f7c735216
s20972 <s20972@pjwstk.edu.pl>
2023-02-02 19:28:54 +0100

    fix

--- src/components/PageService/Amentity.js
+++ src/components/PageService/Amentity.js
@@ -6,7 +6,9 @@ const Amentity = (props) => {
   const data = props.amentity.icon.bytes;
   return (
     <div className={style.bonus}>
-      <Image data={data}></Image>
+      <div className={style.amentity_container}>
+        <Image data={data} className={style.amentity}></Image>
+      </div>
       {/* <ion-icon name="wifi-outline" class={style.bonus__icon} /> */}
       <p className={style.bonus__text}>{props.amentity.name}</p>
     </div>
